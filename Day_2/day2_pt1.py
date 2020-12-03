155c7e6b3ce1a64e9d9474530c6773d8f843820f
toxicscorpius <toxicscorpius@gmail.com>
2020-12-02 21:43:35 -0500

    Moves the for loop (shorter code)

--- Day_2/day2_pt1.py
+++ Day_2/day2_pt1.py
@@ -1,17 +1,15 @@
 import re
 
-with open("day2_input.txt") as file:
-    strings_list = [line.strip() for line in file]
-
 regex = re.compile(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$")
 n_correct_pwd = 0
 
-for line in strings_list:
-    groups_p = re.match(regex, line)
-    if groups_p is not None:
-        min_n, max_n, char, pwd = groups_p.groups()
-        min_n, max_n = int(min_n), int(max_n)
-        if max_n >= pwd.count(char) >= min_n:
-            n_correct_pwd += 1
+with open("day2_input.txt") as file:
+    for line in file:
+        groups_p = re.match(regex, line)
+        if groups_p is not None:
+            min_n, max_n, char, pwd = groups_p.groups()
+            min_n, max_n = int(min_n), int(max_n)
+            if max_n >= pwd.count(char) >= min_n:
+                n_correct_pwd += 1
 
 print(n_correct_pwd)
--- Day_2/day2_pt2.py
+++ Day_2/day2_pt2.py
@@ -1,17 +1,15 @@
 import re
 
-with open("day2_input.txt") as file:
-    strings_list = [line.strip() for line in file]
-
 regex = re.compile(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$")
 n_correct_pwd = 0
 
-for line in strings_list:
-    groups_p = re.match(regex, line)
-    if groups_p is not None:
-        pos1, pos2, char, pwd = groups_p.groups()
-        pos1, pos2 = int(pos1) - 1, int(pos2) - 1
-        if (char == pwd[pos1]) ^ (char == pwd[pos2]):
-            n_correct_pwd += 1
+with open("day2_input.txt") as file:
+    for line in file:
+        groups_p = re.match(regex, line)
+        if groups_p is not None:
+            pos1, pos2, char, pwd = groups_p.groups()
+            pos1, pos2 = int(pos1) - 1, int(pos2) - 1
+            if (char == pwd[pos1]) ^ (char == pwd[pos2]):
+                n_correct_pwd += 1
 
 print(n_correct_pwd)
