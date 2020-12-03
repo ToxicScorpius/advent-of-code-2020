d307dc41b05443aa2d204e9a89351de45c3fda60
toxicscorpius <toxicscorpius@gmail.com>
2020-12-03 17:17:06 -0500

    Shortens significantly the code with a function

--- Day_3/day3_pt1.py
+++ Day_3/day3_pt1.py
@@ -1,12 +1,3 @@
-n, trees = 0, 0
+from day3_function import check_trees
 
-with open("day3_input.txt") as file:
-    for line in file:
-        line = line.strip()
-        if line[n] == "#":
-            trees += 1
-        n += 3
-        if n > len(line) - 1:
-            n -= len(line)
-
-print(trees)
+print(check_trees(3, 1))
--- Day_3/day3_pt2.py
+++ Day_3/day3_pt2.py
@@ -1,54 +1,6 @@
-n1, n2, n3, n4, n5 = 0, 0, 0, 0, 0
-s1_trees, s2_trees, s3_trees, s4_trees, s5_trees = 0, 0, 0, 0, 0
-line_count = 0
+from day3_function import check_trees
 
-with open("day3_input.txt") as file:
-    for line in file:
-        line = line.strip()
-        if line[n1] == "#":
-            s1_trees += 1
-        if line[n2] == "#":
-            s2_trees += 1
-        if line[n3] == "#":
-            s3_trees += 1
-        if line[n4] == "#":
-            s4_trees += 1
-        if line_count % 2 == 0:
-            if line[n5] == "#":
-                s5_trees += 1
-
-        n1 += 1
-        n2 += 3
-        n3 += 5
-        n4 += 7
-        if line_count % 2 == 0:
-            n5 += 1
-
-        if n1 > len(line) - 1:
-            n1 -= len(line)
-        if n2 > len(line) - 1:
-            n2 -= len(line)
-        if n3 > len(line) - 1:
-            n3 -= len(line)
-        if n4 > len(line) - 1:
-            n4 -= len(line)
-        if n5 > len(line) - 1:
-            n5 -= len(line)
-
-        line_count += 1
-product = s1_trees * s2_trees * s3_trees * s4_trees * s5_trees
-
-
-class ProductValidity(Exception):
-    """Checks the validity of the product."""
-
-
-try:
-    if product != 6708199680:
-        raise ProductValidity()
-except ProductValidity:
-    print(f"{product} != 6708199680")
-    print("Invalid product.")
-
-if product == 6708199680:
-    print(f"{s1_trees}*{s2_trees}*{s3_trees}*{s4_trees}*{s5_trees} = {product}")
+p1, p2, p3 = check_trees(1, 1), check_trees(3, 1), check_trees(5, 1)
+p4, p5 = check_trees(7, 1), check_trees(1, 2)
+product = p1 * p2 * p3 * p4 * p5
+print(product)
