de0d57f3f8b661a4f9caa0022cb0cd564e361573
toxicscorpius <toxicscorpius@gmail.com>
2020-12-04 09:33:21 -0500

    Improves the docstring style

--- Day_3/day3_function.py
+++ Day_3/day3_function.py
@@ -1,6 +1,8 @@
 def check_trees(steps_right, steps_down):
-    """Checks whether there is a tree on index [n], increments the tree count
-    and resets the line if necessary, then returns the tree count"""
+    """
+    Check whether there is a tree on index [n], increment the tree count
+    and reset the line if necessary, then return the tree count.
+    """
     lines = [line.strip() for line in open("day3_input.txt")]
     trees_count, index, line_count = 0, 0, 0
     for line in lines:
